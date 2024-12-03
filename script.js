cc94279754c153f8dd2ab990531b5a6950e18c36
sonjaz83 <zivkovic.sonja@gmail.com>
2024-12-03 17:04:12 +0100

    dodato pokretanje funkcije na enter

--- script.js
+++ script.js
@@ -26,10 +26,23 @@ function generate() {
     //deo koji se mnozi: 0 je crna, 255 je bela
     let colorPart = 255 / (inputValue - 1); //-1, jer ako npr. ima 3 elementa ide 0, 255/2, 255
     let colorNum = colorPart * i;
-
+    
     colorStep.style.backgroundColor = 'rgb(' + colorNum + ',' + colorNum + ',' + colorNum + ')';
+
+    if(colorNum < 126){
+      colorStep.style.color = 'rgb(255,255,255)';
+    }else{
+      colorStep.style.color = 'rgb(0,0,0)';
+    }
 
     boxWrapper.appendChild(colorStep);
 
   }
 }
+document.getElementById("inputBtnVal").addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+      // Check if Enter key is pressed
+      document.getElementById("generateBtn").click();
+      // Trigger button click
+  }
+});
